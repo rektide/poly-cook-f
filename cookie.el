1c0954bc6fdf7a6e98822380c151401ee239286c
rektide <rektide@voodoowarez.com>
2013-07-15 20:36:42 -0400

    Apparently properly use Object.create, ? ?

--- cookie.el
+++ cookie.el
@@ -1,10 +1,10 @@
 <element name="x-cookie" constructor="XCookie">
-var XCookie= Object.create(HTMLElement.prototype)
-XCookie.readyCallback= function(){
+var XCookieProto= Object.create(HTMLElement.prototype)
+XCookieProto.readyCallback= function(){
 	var k= this.dataset.key,
 	  v= this.dataset.value
 	document.cookie= k+"="+v
 }
 // todo: you fucked with it detection, kicking out events
-this.register("cookie",{prototype:XCookie})
+var XCookie= this.register("cookie",{prototype:XCookieProto})
 </element>
